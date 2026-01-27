20517ecfb3e18428e4d40f122a8fd742e22e1df1
Dmitrii Bakaev <bakaevdima4@gmail.com>
2026-01-27 13:27:15 +0800

    Added rounding and fixed a minor mistake

--- MarketPerformance2.py
+++ MarketPerformance2.py
@@ -22,7 +22,7 @@ for i in data.index:
         d_ohlcv = bybit.fetch_ohlcv(ticker, timeframe="1d", since=since, limit=31)
     except:
         print("No data for", ticker)
-        data[i, 'is_delisted'] = 1
+        data.at[i, 'is_delisted'] = 1
         continue
 
     if m_ohlcv and h_ohlcv and d_ohlcv and len(d_ohlcv) == 31:
@@ -31,11 +31,12 @@ for i in data.index:
         one_day_price = d_ohlcv[0][4]
         one_month_price = d_ohlcv[30][4]
 
-        data.at[i, 'one_hour_post_listing_performance'] = (one_hour_price - base_price) / base_price
-        data.at[i, 'one_day_post_listing_performance'] = (one_day_price - base_price) / base_price
-        data.at[i, 'one_month_post_listing_performance'] = (one_month_price - base_price)/base_price
+        data.at[i, 'one_hour_post_listing_performance'] = round((one_hour_price - base_price) / base_price, 2)
+        data.at[i, 'one_day_post_listing_performance'] = round((one_day_price - base_price) / base_price, 2)
+        data.at[i, 'one_month_post_listing_performance'] = round ((one_month_price - base_price)/base_price, 2)
         print("found data for", ticker)
     else:
         print("Not enough data for", ticker)
 
 data.to_csv("listing+market_data.csv", index=False)
+print(data.head(5))
