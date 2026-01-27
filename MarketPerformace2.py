721e7852ec31f3c223b0f7769ab0ffdb23acb33c
Dmitrii Bakaev <bakaevdima4@gmail.com>
2026-01-27 13:16:16 +0800

    added is_delisted feature

--- MarketPerformace2.py
+++ MarketPerformace2.py
@@ -3,5 +3,30 @@ import pandas as pd
 
 bybit = ccxt.bybit()
 
-data = pd.read_csv("data.csv")
-print(data)
+for i in data.index:
+    if data['asset_type'][i] == 0: ticker = data['ticker'][i] + "/USDT"
+    else: ticker = data['ticker'][i]
+
+    try:
+        m_ohlcv = bybit.fetch_ohlcv(ticker, timeframe="1m", since=since, limit=1)
+        h_ohlcv = bybit.fetch_ohlcv(ticker, timeframe="1h", since=since, limit=1)
+        d_ohlcv = bybit.fetch_ohlcv(ticker, timeframe="1d", since=since, limit=31)
+    except:
+        print("No data for", ticker)
+        data[i, 'is_delisted'] = 1
+        continue
+
+    if m_ohlcv and h_ohlcv and d_ohlcv and len(d_ohlcv) == 31:
+        base_price = m_ohlcv[0][4]
+        one_hour_price = h_ohlcv[0][4]
+        one_day_price = d_ohlcv[0][4]
+        one_month_price = d_ohlcv[30][4]
+
+        data.at[i, 'one_hour_post_listing_performance'] = (one_hour_price - base_price) / base_price
+        data.at[i, 'one_day_post_listing_performance'] = (one_day_price - base_price) / base_price
+        data.at[i, 'one_month_post_listing_performance'] = (one_month_price - base_price)/base_price
+        print("found data for", ticker)
+    else:
+        print("Not enough data for", ticker)
+
+data.to_csv("listing+market_data.csv", index=False)
