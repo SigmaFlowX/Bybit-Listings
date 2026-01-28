ee88c74588b4f44939558ad3018d47f4219631a4
Dmitrii Bakaev <bakaevdima4@gmail.com>
2026-01-28 13:41:11 +0800

    Minor improvements

--- PreListingData3.py
+++ PreListingData3.py
@@ -7,7 +7,7 @@ pd.set_option("display.width", 1000)
 pd.set_option("display.expand_frame_repr", False)
 
 
-data = pd.read_csv("listing+market_data.csv")
+data = pd.read_csv("listing+post_data.csv")
 
 #add coingecko-id for each ticker
 url = "https://api.coingecko.com/api/v3/coins/list"
@@ -18,66 +18,6 @@ symbol_to_id = {coin['symbol'].lower(): coin['id'] for coin in coins}
 data['coingecko_id'] = data['base_coin'].str.lower().map(symbol_to_id)
 
 
-# #getting prelisting marketcap data
-# data = pd.read_csv("listing+market+prelisting_data.csv")
-# #data['market_cap_minus1d'] = None
-# for i in range(550, 650):
-#     token = data['base_coin'][i]
-#     token_id = data['coingecko_id'][i]
-#     if pd.isna(data['coingecko_id'][i]):continue
-#     if not pd.isna(data['accurate_listing_time'][i]):
-#         listing_date = data['accurate_listing_time'][i]
-#     else:
-#         listing_date = data['listing_time'][i]
-#
-#
-#
-#     # taking -1 day to ensure no look-ahead bias
-#     listing_date = dt = datetime.fromisoformat(listing_date)
-#     listing_date = listing_date.date()
-#     listing_date_minus1 = listing_date - timedelta(days=1)
-#
-#     url = f"https://api.coingecko.com/api/v3/coins/{token_id}/history"
-#     params = {
-#         "date": listing_date_minus1,
-#         "localization": "false",
-#         "x_cg_demo_api_key": "CG-Qz4b9yrLigchwdX3dcgHMp8Q"
-#     }
-#
-#     while True:
-#         try:
-#             r = requests.get(url, params=params)
-#             r.raise_for_status()
-#             inf = r.json()
-#             market_cap = inf.get("market_data", {}).get("market_cap", {}).get('usd')
-#             if market_cap is None or market_cap == 0.0:
-#                 time.sleep(3)
-#                 params = {
-#                     "date": listing_date,
-#                     "localization": "false",
-#                     "x_cg_demo_api_key": "CG-Qz4b9yrLigchwdX3dcgHMp8Q"
-#                 }
-#                 r = requests.get(url, params=params)
-#                 r.raise_for_status()
-#                 inf = r.json()
-#                 market_cap = inf.get("market_data", {}).get("market_cap", {}).get('usd')
-#
-#
-#             data.at[i, 'market_cap_minus1d'] = market_cap
-#             data.to_csv("listing+market+prelisting_data.csv", index=False)
-#             break
-#         except requests.exceptions.HTTPError as e:
-#             if r.status_code == 401:
-#                 print("asdasd")
-#                 market_cap = None
-#                 break
-#         except Exception as e:
-#             print(e)
-#             time.sleep(60)
-#
-#     print(i)
-#     print(token, market_cap)
-#     time.sleep(2)
 
 
 data['marketcap_minus1d'] = None
@@ -104,10 +44,24 @@ for i in data.index:
         "to": pd.to_datetime(listing_date).date(),
         "x_cg_demo_api_key": "CG-Qz4b9yrLigchwdX3dcgHMp8Q",
     }
-    print(listing_date)
-    r = requests.get(url, params=params)
-    r.raise_for_status()
-    inf = r.json()
+
+    while True:
+        try:
+            r = requests.get(url, params=params)
+            r.raise_for_status()
+            inf = r.json()
+            break
+        except requests.exceptions.HTTPError as e:
+            if r.status_code == 401:
+                print(f"401 for token {token_id} \n Probably out of 1 year range (coingecko limit). Further search is useless, you can stop now")
+                break
+        except Exception as e:
+            print("Unexpected error in request, sleeping")
+            time.sleep(60)
+
+    if not inf.get("prices"):
+        print(f"Empty data for token {token_id}")
+        continue
 
     df_prices = pd.DataFrame(inf["prices"], columns=["timestamp", "price"])
     df_mc = pd.DataFrame(inf["market_caps"], columns=["timestamp", "market_cap"])
@@ -144,5 +98,7 @@ for i in data.index:
         data.at[i, 'price_change_minus7d'] = price_change_minus7d
         data.at[i, 'volume_change_minus7d'] = volume_change_minus7d
 
-    data.to_csv("listing+market+prelisting_data.csv", index=False)
+    data.to_csv("listing+post+pre_data.csv", index=False)
+    print(f"Done for token {token_id}")
+    print(i)
     time.sleep(2)
